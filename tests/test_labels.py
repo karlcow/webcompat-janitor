e5f8dcaee5e72410303c39be9759743b6d239b53
Karl Dubost <karl+github@la-grange.net>
2017-08-03 15:51:55 +0900

    Adds tests for different validation scenarios

--- tests/test_labels.py
+++ tests/test_labels.py
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-'''Tests for Webcompat Janitor Business Rules.'''
+"""Tests for Webcompat Janitor Business Rules."""
 
 import json
 import unittest
@@ -22,17 +22,51 @@ class TestJanitorLabels(unittest.TestCase):
         pass
 
     def read_issue(self, issue_fixture):
-        '''Reads the fixture for a test.'''
+        """Reads the fixture for a test."""
         with open(FIXTURE_DIR + issue_fixture) as f:
             json_issue = json.load(f)
         return json_issue
 
-    def test_has_no_status_label(self):
-        '''Send an error when issue has no status label.'''
+    def test_get_status_labels(self):
+        """Returns the list of labels."""
+        json_issue = self.read_issue('issue_with_status_label.json')
+        expected = ['status-needstriage']
+        actual = validation.get_status_labels(json_issue)
+        self.assertListEqual(expected, actual)
         json_issue = self.read_issue('issue_no_status_label.json')
-        expected = (False, 1)
+        actual = validation.get_status_labels(json_issue)
+        self.assertIsNone(actual)
+
+    def test_has_status_label(self):
+        """Send True when issue has status label, else False."""
+        json_issue = self.read_issue('issue_no_status_label.json')
         actual = validation.has_status_label(json_issue)
-        self.assertTupleEqual(expected, actual)
+        self.assertFalse(actual)
+        json_issue = self.read_issue('issue_with_status_label.json')
+        actual = validation.has_status_label(json_issue)
+        self.assertTrue(actual)
+
+    def test_has_conflicting_status(self):
+        """Send True for mutually exclusive labels."""
+        json_issue = self.read_issue('issue_multiple_status_labels.json')
+        actual = validation.has_conflicting_status(json_issue)
+        self.assertTrue(actual)
+        json_issue = self.read_issue('issue_with_status_label.json')
+        actual = validation.has_wrong_status(json_issue)
+        self.assertFalse(actual)
+
+    def test_has_wrong_status(self):
+        """Send True for issues with wrong labels with regards to state."""
+        json_issue = self.read_issue('issue_wrong_open_status_label.json')
+        actual = validation.has_wrong_status(json_issue)
+        self.assertTrue(actual)
+        json_issue = self.read_issue('issue_wrong_closed_status_label.json')
+        actual = validation.has_wrong_status(json_issue)
+        self.assertTrue(actual)
+        json_issue = self.read_issue('issue_with_status_label.json')
+        actual = validation.has_wrong_status(json_issue)
+        self.assertFalse(actual)
+
 
 
 if __name__ == '__main__':
